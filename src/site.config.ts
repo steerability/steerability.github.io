f450cb001d5ab02255f809f7827258ce8b3baeb4
steerability <steerability@googlegroups.com>
2025-11-13 13:08:41 +0000

    Update site.config.ts

--- src/site.config.ts
+++ src/site.config.ts
@@ -25,16 +25,29 @@ export const siteConfig: SiteConfig = {
 		- The header link text
 		- Footer text
 	*/
-	title: "AAAI 2026 Lab: Learning to Steer LLMs",
+	title: "Steerability Lab",
+	// Full public URL of this specific site
 	url: "https://steerability.github.io/lab",
 };
 
 // Used to generate links in both the Header & Footer.
-export const menuLinks = [
-  { path: "/lab/", title: "Home" },
-  { path: "/lab/about/", title: "About" },
-  { path: "/lab/posts/", title: "Blog" },
-  { path: "/lab/notes/", title: "Notes" },
+export const menuLinks: { path: string; title: string }[] = [
+	{
+		path: "/",
+		title: "Home",
+	},
+	{
+		path: "/about/",
+		title: "About",
+	},
+	{
+		path: "/posts/",
+		title: "Blog",
+	},
+	{
+		path: "/notes/",
+		title: "Notes",
+	},
 ];
 
 // https://expressive-code.com/reference/configuration/
@@ -42,4 +55,27 @@ export const expressiveCodeOptions: AstroExpressiveCodeOptions = {
 	styleOverrides: {
 		borderRadius: "4px",
 		codeFontFamily:
-			'ui-monospace, SFMono-Regular, Menlo, Monaco, Consolas, "Liberation Mono", "Cou
+			'ui-monospace, SFMono-Regular, Menlo, Monaco, Consolas, "Liberation Mono", "Courier New", monospace',
+		codeFontSize: "0.875rem",
+		codeLineHeight: "1.7142857rem",
+		codePaddingInline: "1rem",
+		frames: {
+			frameBoxShadowCssValue: "none",
+		},
+		uiLineHeight: "inherit",
+	},
+	themeCssSelector(theme, { styleVariants }) {
+		// If one dark and one light theme are available
+		// generate theme CSS selectors compatible with cactus-theme dark mode switch
+		if (styleVariants.length >= 2) {
+			const baseTheme = styleVariants[0]?.theme;
+			const altTheme = styleVariants.find((v) => v.theme.type !== baseTheme?.type)?.theme;
+			if (theme === baseTheme || theme === altTheme) return `[data-theme='${theme.type}']`;
+		}
+		// return default selector
+		return `[data-theme="${theme.name}"]`;
+	},
+	// One dark, one light theme => https://expressive-code.com/guides/themes/#available-themes
+	themes: ["dracula", "github-light"],
+	useThemedScrollbars: false,
+};
