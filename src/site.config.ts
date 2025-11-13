12c1e5bb5d0cee6e5297d081da6e07bea7d17512
steerability <steerability@googlegroups.com>
2025-11-13 13:23:40 +0000

    Update site.config.ts

--- src/site.config.ts
+++ src/site.config.ts
@@ -2,9 +2,8 @@ import type { SiteConfig } from "@/types";
 import type { AstroExpressiveCodeOptions } from "astro-expressive-code";
 
 export const siteConfig: SiteConfig = {
-	// Used as both a meta property & the generated OG image
 	author: "Steerability",
-	// Date.prototype.toLocaleDateString() parameters, found in src/utils/date.ts.
+
 	date: {
 		locale: "en-GB",
 		options: {
@@ -13,44 +12,23 @@ export const siteConfig: SiteConfig = {
 			year: "numeric",
 		},
 	},
-	// Default description meta property and webmanifest description
+
 	description: "The Steerability lab site.",
-	// HTML lang property
 	lang: "en-GB",
-	// Meta property
 	ogLocale: "en_GB",
-	/*
-		- Used to construct the meta title property
-		- The webmanifest name (via astro.config.ts)
-		- The header link text
-		- Footer text
-	*/
 	title: "Steerability Lab",
-	// Full public URL of this specific site
+
+	// Full public URL of THIS site
 	url: "https://steerability.github.io/lab",
 };
 
-// Used to generate links in both the Header & Footer.
-export const menuLinks: { path: string; title: string }[] = [
-	{
-		path: "/",
-		title: "Home",
-	},
-	{
-		path: "/about/",
-		title: "About",
-	},
-	{
-		path: "/posts/",
-		title: "Blog",
-	},
-	{
-		path: "/notes/",
-		title: "Notes",
-	},
+export const menuLinks = [
+	{ path: "/", title: "Home" },
+	{ path: "/about/", title: "About" },
+	{ path: "/posts/", title: "Blog" },
+	{ path: "/notes/", title: "Notes" },
 ];
 
-// https://expressive-code.com/reference/configuration/
 export const expressiveCodeOptions: AstroExpressiveCodeOptions = {
 	styleOverrides: {
 		borderRadius: "4px",
@@ -59,23 +37,20 @@ export const expressiveCodeOptions: AstroExpressiveCodeOptions = {
 		codeFontSize: "0.875rem",
 		codeLineHeight: "1.7142857rem",
 		codePaddingInline: "1rem",
-		frames: {
-			frameBoxShadowCssValue: "none",
-		},
+		frames: { frameBoxShadowCssValue: "none" },
 		uiLineHeight: "inherit",
 	},
+
 	themeCssSelector(theme, { styleVariants }) {
-		// If one dark and one light theme are available
-		// generate theme CSS selectors compatible with cactus-theme dark mode switch
 		if (styleVariants.length >= 2) {
 			const baseTheme = styleVariants[0]?.theme;
 			const altTheme = styleVariants.find((v) => v.theme.type !== baseTheme?.type)?.theme;
-			if (theme === baseTheme || theme === altTheme) return `[data-theme='${theme.type}']`;
+			if (theme === baseTheme || theme === altTheme)
+				return `[data-theme='${theme.type}']`;
 		}
-		// return default selector
 		return `[data-theme="${theme.name}"]`;
 	},
-	// One dark, one light theme => https://expressive-code.com/guides/themes/#available-themes
+
 	themes: ["dracula", "github-light"],
 	useThemedScrollbars: false,
 };
